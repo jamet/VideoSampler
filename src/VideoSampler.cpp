0e869fe43864d83b4a98e8f12a989d8c10ec5e18
jamet <acryl@live.fr>
2015-02-25 17:12:50 +0100

    more code

--- src/VideoSampler.cpp
+++ src/VideoSampler.cpp
@@ -12,40 +12,31 @@ void VideoSampler::draw(){
     cout<<"draw"<<endl;
         playModes.draw();
         playModes.drawData();
-                if (playModes.vBuffer.getNextVideoFrame()!= NULL){
-                    playModes.vBuffer.getNextVideoFrame().getTextureRef().draw(640 , 480, 960, 720);
-                }else cout<<"nulllllllllllllllllllllllllllll"<<endl;
+                if (playModes.vBuffer.getVideoFrame(playHead)!= NULL){
+
+                    playModes.vBuffer.getVideoFrame(playHead).getTextureRef().draw(640 , 0, 320, 240);
+                    cout<<"timestamp "<<playModes.vBuffer.getVideoFrame(playHead).getTimestamp().epochMicroseconds()<<endl;
+
+                }
 
                 cout<<"setframepos"<<playHead<<" framepos "<<playModes.vBuffer.framePos<<endl;
-                playModes.vBuffer.iterFramePos();
+                //playModes.vBuffer.iterFramePos();
                 cout<<"iterframepos"<<playHead<<" framepos "<<playModes.vBuffer.framePos<<endl;
 
 
- /*       // draw a playhead over the waveform
-    ofSetColor(ofColor::white);
-    ofLine(left + ps->getNormalisedPosition() * waveformWidth, top, left + ps->getNormalisedPosition() * waveformWidth, top + waveformHeight);
-    ofDrawBitmapString("PlayHead", left + ps->getNormalisedPosition() * waveformWidth-69, top+30);
-
-    // Draw Current Recording Position
-    ofSetColor(ofColor::red);
-    ofLine(left + getRecordPostion() * waveformWidth, top, left + getRecordPostion() * waveformWidth, top + waveformHeight);
-    ofDrawBitmapString("RecPos", left + getRecordPostion() * waveformWidth-52, top+15);
-
-    // draw a frame around the whole thing
-    ofSetColor(ofColor::white);
-    ofNoFill();
-    ofRect(left, top, waveformWidth, waveformHeight);*/
 }
 
 
 void VideoSampler::update(){
         cout<<"update"<<endl;
         //playHead=playStart;
-cout<<"record position++"<<recordPosition<<" framepos "<<playModes.vBuffer.framePos<<endl;
+cout<<"record position u"<<recordPosition<<" framepos "<<playModes.vBuffer.framePos<<endl;
         //playModes.update();
         if (bRecLiveInput){
+            playModes.vBuffer.resume();
             playModes.vBuffer.setFramePos(getRecordPostion()/100);
-            playHead=playStart;
+            //playHead=playStart;
+
             //increment recordPosition
             if (recordPosition<100){
                 //playEnd=recordPosition/100;
@@ -59,7 +50,7 @@ cout<<"record position++"<<recordPosition<<" framepos "<<playModes.vBuffer.frame
                 //playHead=playStart;
                 bRecLiveInput=false;
                 bPlayBuffer=true;
-                playModes.vBuffer.setFramePos(playHead);
+                //playModes.vBuffer.setFramePos(playHead);
 
             }
 
@@ -67,69 +58,48 @@ cout<<"record position++"<<recordPosition<<" framepos "<<playModes.vBuffer.frame
         {
 
             playModes.vBuffer.stop();
-            playModes.vBuffer.setFramePos(playHead);
+            //playModes.vBuffer.setFramePos(playHead);
             //playModes.vBuffer.draw();
             if (bPlayBuffer){
-                //playModes.vBuffer.setFramePos(playHead);
-              /*  if (playModes.vBuffer.getNextVideoFrame()!= NULL){
-                    playModes.vBuffer.getNextVideoFrame().getTextureRef().draw(640 , 480, 960, 720);
-                }else cout<<"nulllllllllllllllllllllllllllll"<<endl;
-
-                cout<<"setframepos"<<playHead<<" framepos "<<playModes.vBuffer.framePos<<endl;
-                playModes.vBuffer.iterFramePos();
-                cout<<"iterframepos"<<playHead<<" framepos "<<playModes.vBuffer.framePos<<endl;*/
 
                 updatePlayHead();
+
             }
 
         }
 
     cout<<"record position"<<getRecordPostion()<<endl;
-    playModes.setSpeed(speed);
+    //playModes.setSpeed(speed);
 
 
     playModes.update();
+    cout<<"header loop"<<playModes.vHeader.getLoopMode()<< " header is playing "<<playModes.vHeader.isPlaying()<<endl;
 
-   /* if(bRecLiveInput==false){
-        if(!grainPlayer.bSetPosition==true){
-            playModes.setDelay(grainPlayer.ps->getNormalisedPosition());
-        }
-    }
-    else if(grainPlayer.bRecLiveInput==true){
-        playModes.setDelay(grainPlayer.ps->getNormalisedPosition());
-    }*/
+
 }
 
 void VideoSampler::updatePlayHead(){
-    /*if(!bSetPosition) {
-        if(bUpdatePlayheadEvent==true){
-            ps->setPosition(ofMap(samp.recordPosition,0,LENGTH,0.0,1.0));
-        }
-        bUpdatePlayheadEvent = false;
-    } else {
-        bUpdatePlayheadEvent = true;
-    }
-*/
+
     if (playHead<playEnd){
 
         playHead+=0.01;
         cout<<"playhead++"<<playHead<<endl;
-        //playModes.vBuffer.iterFramePos();
-        //playModes.vRenderer.
+
     }else {
+
         playHead=playStart;
-        //playModes.vBuffer.setFramePos(playHead);
 
         bRecLiveInput=false;
     }
 
-    playModes.vBuffer.setFramePos(playHead);
 
     if(ofGetFrameNum()==100){
         speed = 1.0;
     }
 
-    cout<<"playhead"<<playHead<<endl;
+
+
+    cout<<"playhead updated==========================="<<playHead<<"buffer framepos"<<playModes.vBuffer.getFramePos()<<endl;
 }
 
 float VideoSampler::getRecordPostion(){
